0fdae860f6ffe60cf5c4adeb8bd5a3798f4d3355
Steven Shih <stevenshih1997@gmail.com>
2017-09-23 14:44:55 -0700

    database setup -start

---
+++ knex.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {  
+    client: 'postgresql',
+      connection: {
+        host     : process.env.DB_HOST,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : process.env.DB_NAME,
+        port     : process.env.DB_PORT,
+      },
+      migrations: {
+        directory: './db/migrations',
+        tableName: 'migrations'
+      },
+      seeds: {
+        directory: './db/seeds' 
+      }
+    }
+  }
+}
+
