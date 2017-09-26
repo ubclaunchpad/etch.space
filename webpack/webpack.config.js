5d0554d143f83144d9673106cdc0377138ac082f
Tristan <tristancalderbank@gmail.com>
2017-09-26 14:15:36 -0700

    adding react

---
+++ webpack/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: __dirname + '/../client/index.js',
+    output: {
+        path: __dirname + '/../public/js',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['react', 'es2015']
+                  }
+                }
+            }
+        ]
+    }
+};
