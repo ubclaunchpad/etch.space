d4ba69539a5ee3dacabfb33db00b46ab0d76515f
Tristan <tristancalderbank@gmail.com>
2017-10-10 22:18:38 -0700

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const webpack = require('webpack');
+
 module.exports = {
     entry: __dirname + '/client/index.js',
     output: {
         path: __dirname + '/public/js',
         filename: "bundle.js"
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+        })
+    ],
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" },
